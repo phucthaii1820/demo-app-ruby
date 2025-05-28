4f45c4e2660f170473baf3aa1e3350629b199ca4
Arthur's Macbook <arthur@Arthurss-MacBook-Pro.local>
2025-05-28 11:28:01 +0700

    Refactor update action in Users::RegistrationsController to handle name and password updates with appropriate responses

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,8 +3,36 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   include RackSessionsFix
 
-  private
+  def update
+    if params[:user][:new_password].present?
+      unless current_user.valid_password?(params[:user][:current_password])
+        return render json: {
+          status: { code: 401, message: "Current password is incorrect." },
+          data: {}
+        }, status: :unauthorized
+      end
 
+      if current_user.update(name: params[:user][:name], password: params[:user][:new_password])
+        render json: {
+          status: { code: 200, message: "Name and password updated successfully." },
+          data: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
+        }
+      else
+        render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      if current_user.update(name: params[:user][:name])
+        render json: {
+          status: { code: 200, message: "Name updated successfully." },
+          data: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
+        }
+      else
+        render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  end
+
+  private
   def respond_with(current_user, _opts = {})
     if resource.persisted?
       render json: {
